04be1f6ed627c0b94248eb10c5d7af1bf0a01abf
Santiago <santiago.valle1245@gmail.com>
2026-03-22 16:55:55 -0600

    Refactorización: ContentView centrado y con fondo animado =)

---
+++ LECO/FondoAnimadoView.swift
@@ -0,0 +1,18 @@
+//
+//  FondoAnimadoView.swift
+//  LECO
+//
+//  Created by Marcela Valle Aguirre on 22/03/26.
+//
+
+import SwiftUI
+
+struct FondoAnimadoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FondoAnimadoView()
+}
