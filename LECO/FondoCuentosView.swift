b6ac17222503df46f76b5baf2b0f4a77b8c95f2e
Santiago <santiago.valle1245@gmail.com>
2026-03-22 18:54:54 -0600

    Cambio de fondos en los menus principales de la App =)

---
+++ LECO/FondoCuentosView.swift
@@ -0,0 +1,18 @@
+//
+//  FondoCuentosView.swift
+//  LECO
+//
+//  Created by Marcela Valle Aguirre on 22/03/26.
+//
+
+import SwiftUI
+
+struct FondoCuentosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FondoCuentosView()
+}
